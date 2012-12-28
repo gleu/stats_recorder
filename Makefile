dabfe17646a51217b95c7da276a3b81d4532b77e
Guillaume Lelarge <guillaume@lelarge.info>
2012-12-28 16:32:58 +0100

    First release

---
+++ Makefile
@@ -0,0 +1,7 @@
+# contrib/stats_recorder_spi/Makefile
+
+MODULES = stats_recorder_spi
+
+PG_CONFIG = pg_config
+PGXS := $(shell $(PG_CONFIG) --pgxs)
+include $(PGXS)
---
+++ stats_recorder_spi.c
@@ -0,0 +1,241 @@
+/* -------------------------------------------------------------------------
+ *
+ * stats_recorder_spi.c
+ *		Background worker code that gets a periodic copy of stats.
+ *
+ *
+ * Original code from PostgreSQL Global Development Group
+ *   (worker_spi contrib module)
+ *
+ * Copyright (c) 2012, Guillaume Lelarge (Dalibo),
+ * guillaume.lelarge@dalibo.com
+ *
+ * -------------------------------------------------------------------------
+ */
+#include "postgres.h"
+
+/* These are always necessary for a bgworker */
+#include "miscadmin.h"
+#include "postmaster/bgworker.h"
+#include "storage/ipc.h"
+#include "storage/latch.h"
+#include "storage/lwlock.h"
+#include "storage/proc.h"
+#include "storage/shmem.h"
+
+/* these headers are used by this particular worker's code */
+#include "access/xact.h"
+#include "executor/spi.h"
+#include "fmgr.h"
+#include "lib/stringinfo.h"
+#include "utils/builtins.h"
+#include "utils/snapmgr.h"
+#include "tcop/utility.h"
+
+PG_MODULE_MAGIC;
+
+static char *  stats_recorder_schema = "stats_recorder";
+static int     stats_recorder_naptime = 1;
+static bool    debug = true;
+
+void	_PG_init(void);
+
+static bool	got_sigterm = false;
+
+static void
+worker_spi_sigterm(SIGNAL_ARGS)
+{
+	int			save_errno = errno;
+
+	if (debug)
+		elog(LOG, "%s, worker_spi_sigterm", MyBgworkerEntry->bgw_name);
+
+	elog(LOG, "%s shutting down", MyBgworkerEntry->bgw_name);
+	got_sigterm = true;
+	if (MyProc)
+		SetLatch(&MyProc->procLatch);
+
+	errno = save_errno;
+}
+
+static void
+worker_spi_sighup(SIGNAL_ARGS)
+{
+	if (debug)
+		elog(LOG, "%s, worker_spi_sighup", MyBgworkerEntry->bgw_name);
+
+	if (MyProc)
+		SetLatch(&MyProc->procLatch);
+}
+
+static void
+initialize_worker_spi()
+{
+	int		ret;
+	int		ntup;
+	bool	isnull;
+	StringInfoData	buf;
+
+	if (debug)
+		elog(LOG, "%s, initialize_worker_spi", MyBgworkerEntry->bgw_name);
+
+	StartTransactionCommand();
+	SPI_connect();
+	PushActiveSnapshot(GetTransactionSnapshot());
+
+	initStringInfo(&buf);
+	appendStringInfo(&buf, "select count(*) from pg_namespace where nspname = '%s'",
+					 stats_recorder_schema);
+
+	ret = SPI_execute(buf.data, true, 0);
+	if (ret != SPI_OK_SELECT)
+		elog(FATAL, "SPI_execute failed: error code %d", ret);
+
+	if (SPI_processed != 1)
+		elog(FATAL, "not a singleton result");
+
+	ntup = DatumGetInt32(SPI_getbinval(SPI_tuptable->vals[0],
+									   SPI_tuptable->tupdesc,
+									   1, &isnull));
+	if (isnull)
+		elog(FATAL, "null result");
+
+	if (ntup == 0)
+	{
+		resetStringInfo(&buf);
+		appendStringInfo(&buf,
+						 "CREATE SCHEMA \"%s\" "
+						 "CREATE TABLE \"stat_bgwriter\" (dlog timestamp, LIKE pg_stat_bgwriter)",
+						 stats_recorder_schema);
+
+		ret = SPI_execute(buf.data, false, 0);
+
+		if (ret != SPI_OK_UTILITY)
+			elog(FATAL, "failed to create my schema");
+	}
+
+	SPI_finish();
+	PopActiveSnapshot();
+	CommitTransactionCommand();
+}
+
+static void
+worker_spi_main(void *main_arg)
+{
+	StringInfoData	buf;
+
+	if (debug)
+		elog(LOG, "%s, worker_spi_main", MyBgworkerEntry->bgw_name);
+
+	/* We're now ready to receive signals */
+	BackgroundWorkerUnblockSignals();
+
+	/* Connect to our database */
+	BackgroundWorkerInitializeConnection("postgres", NULL);
+
+	elog(LOG, "%s initialized with schema %s",
+		 MyBgworkerEntry->bgw_name, stats_recorder_schema);
+	initialize_worker_spi();
+
+	/*
+	 * Quote identifiers passed to us.  Note that this must be done after
+	 * initialize_worker_spi, because that routine assumes the names are not
+	 * quoted.
+	 *
+	 * Note some memory might be leaked here.
+	 */
+	initStringInfo(&buf);
+	appendStringInfo(&buf,
+					 "INSERT INTO %s.stat_bgwriter "
+					 "SELECT clock_timestamp(), * FROM pg_stat_bgwriter",
+					 quote_identifier(stats_recorder_schema));
+
+	while (!got_sigterm)
+	{
+		int		ret;
+		int		rc;
+
+		/*
+		 * Background workers mustn't call usleep() or any direct equivalent:
+		 * instead, they may wait on their process latch, which sleeps as
+		 * necessary, but is awakened if postmaster dies.  That way the
+		 * background process goes away immediately in an emergency.
+		 */
+		elog(LOG, "latch %d", stats_recorder_naptime);
+		rc = WaitLatch(&MyProc->procLatch,
+					   WL_LATCH_SET | WL_TIMEOUT | WL_POSTMASTER_DEATH,
+					   stats_recorder_naptime*1000L);
+		ResetLatch(&MyProc->procLatch);
+
+		/* emergency bailout if postmaster has died */
+		if (rc & WL_POSTMASTER_DEATH)
+			proc_exit(1);
+
+		StartTransactionCommand();
+		SPI_connect();
+		PushActiveSnapshot(GetTransactionSnapshot());
+
+		ret = SPI_execute(buf.data, false, 0);
+
+		if (ret != SPI_OK_INSERT)
+			elog(FATAL, "cannot insert into table %s.stat_bgwriter: error code %d",
+				 stats_recorder_schema, ret);
+
+		SPI_finish();
+		PopActiveSnapshot();
+		CommitTransactionCommand();
+	}
+
+	proc_exit(0);
+}
+
+/*
+ * Entrypoint of this module.
+ *
+ * We register one worker process.
+ */
+void
+_PG_init(void)
+{
+	BackgroundWorker	worker;
+
+	if (debug)
+		elog(LOG, "stats_recorder, _PG_init");
+
+	/*   
+ 	 * Define (or redefine) custom GUC variables.
+	 */
+	DefineCustomStringVariable("stats_recorder.schema",
+				"Schema in which we'll store the recorded statistics.",
+				NULL,
+				&stats_recorder_schema,
+				"stats_recorder",
+				PGC_POSTMASTER,
+				0,
+				NULL,
+				NULL,
+				NULL);
+	DefineCustomIntVariable("stats_recorder.naptime",
+				"Duration between each statistics gathering (in seconds).",
+				NULL,
+				&stats_recorder_naptime,
+				1,
+				1,
+				INT_MAX,
+				PGC_SIGHUP,
+				0,
+				NULL,
+				NULL,
+				NULL);
+
+	/* register the worker process */
+	worker.bgw_flags = BGWORKER_SHMEM_ACCESS |
+		BGWORKER_BACKEND_DATABASE_CONNECTION;
+	worker.bgw_start_time = BgWorkerStart_RecoveryFinished;
+	worker.bgw_main = worker_spi_main;
+	worker.bgw_sighup = worker_spi_sighup;
+	worker.bgw_sigterm = worker_spi_sigterm;
+	worker.bgw_name = "stats recorder";
+	worker.bgw_restart_time = BGW_NEVER_RESTART;
+	RegisterBackgroundWorker(&worker);
+}
